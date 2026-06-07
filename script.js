11249e549ade321b30ac2caebc7781f6455934a3
shashikapoor010179-crypto <shashikapoor010179@gmail.com>
2026-06-07 16:44:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const schoolData = {
 
+principal:
+"Mr Sharma",
+
+chemistryTeacher:
+"Mrs Gupta",
+
+class11Fee:
+"₹45000",
+
+busFee:
+"₹1500",
+
+schoolTiming:
+"8:00 AM to 2:00 PM",
+
+holiday:
+"Sunday"
+
+};
